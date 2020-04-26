3e344720bc6b44564e264035eb5e4895be3865f6
rahul499 <rahul4mishra99@gmail.com>
2020-04-26 22:36:28 +0530

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,7 +12,27 @@ renderSquare(i){
    render() {
 
    	return(
-        
+         <div>
+
+          <div className="border-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          </div>
+
+          <div className="border-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          </div>
+
+          <div className="border-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          </div>
+
+        </div>
        
 
    		)
