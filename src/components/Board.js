0e0262979f715917ce8e6e7c1b9d6b8619658371
rahul499 <rahul4mishra99@gmail.com>
2020-04-26 22:36:48 +0530

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,11 +3,7 @@ import Square from './Square';
 
 export default class Board extends Component {
 
-renderSquare(i){
-	return <Square value = {this.props.squares[i]}
-	onClick={()=> this.props.onClick(i)}
-	/>
-   }
+
 
    render() {
 
