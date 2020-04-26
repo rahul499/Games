4d938d9a27cd48e8cab207251b2fb4e95f27b716
rahul499 <rahul4mishra99@gmail.com>
2020-04-26 21:57:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Game from './components/Game.js';
+import Image from './images/hello.jpg';
+
+function App() {
+
+  const styles = {
+    paperContainer: {
+        backgroundImage: `url(${Image})`
+    }
+};
+  return (
+    <div style={styles.paperContainer} className="lkj">
+    <div>
+    <h1 className="text"><center> TIC TAC TOE </center></h1>
+    </div>
+    <div style={styles.paper} className="lkj">
+    <Game />
+    </div>
+    </div>
+  );
+}
+
+export default App;
